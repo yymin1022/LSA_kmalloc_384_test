b63e94e1dacb5f4576905b4993f3d45489e2608e
yymin1022 <yymin1022@gmail.com>
2023-11-19 16:57:43 +0000

    Update Source : Improved Logging Message

--- kmalloc_384_test.c
+++ kmalloc_384_test.c
@@ -12,26 +12,26 @@ static void *test_mem_ptr = NULL;
 
 static int __init test_module_init(void)
 {
-    printk(KERN_INFO "TestModule: Loading\n");
+    printk(KERN_INFO "384 TestModule: Loading\n");
 
     test_mem_ptr = kmalloc(TEST_MEM_SIZE, GFP_KERNEL);
 
     if (!test_mem_ptr) {
-        printk(KERN_ALERT "TestModule: Memory allocation failed\n");
+        printk(KERN_ALERT "384 TestModule: Memory allocation failed\n");
         return -ENOMEM;
     }
 
-    printk(KERN_INFO "TestModule: Memory allocated at %p\n", test_mem_ptr);
+    printk(KERN_INFO "384 TestModule: Memory allocated at %p\n", test_mem_ptr);
     return 0;
 }
 
 static void __exit test_module_exit(void)
 {
-    printk(KERN_INFO "TestModule: Unloading\n");
+    printk(KERN_INFO "384 TestModule: Unloading\n");
 
     if (test_mem_ptr) {
         kfree(test_mem_ptr);
-        printk(KERN_INFO "TestModule: Memory freed\n");
+        printk(KERN_INFO "384 TestModule: Memory freed\n");
     }
 }
 
